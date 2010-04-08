7dc06d7e217f20d3456226a6467a64422bd00edd
JEEN Lee <jeen@perl.kr>
2010-04-08 13:00:03 +0900

    initial commit

---
+++ lib/ibp_.pm
@@ -0,0 +1,32 @@
+package ibp_;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ibp_ -
+
+=head1 SYNOPSIS
+
+  use ibp_;
+
+=head1 DESCRIPTION
+
+ibp_ is
+
+=head1 AUTHOR
+
+JEEN E<lt>jeen@perl.krE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
